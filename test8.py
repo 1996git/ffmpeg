28d68b4001de476b54d3b03a2357746f75e55713
Yuki Shiibashi <141940886+1996git@users.noreply.github.com>
2024-05-02 18:21:34 +0900

    test8.py

--- test8.py
+++ test8.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from tkinter import ttk, filedialog, simpledialog
 from moviepy.editor import VideoFileClip
@@ -14,11 +13,202 @@ import subprocess
 import pandas as pd
 import ffmpeg
 import ctypes
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "your_service_account_key.json"
 
-selected_video_paths = []
+class VideoPlayer:
+    def __init__(self, parent, width=800, height=500, video_width=600, video_height=500):
+        self.parent = parent
+        self.canvas = tk.Canvas(parent, bg="black", width=width, height=height)
+        self.canvas.pack(expand=True, fill="both")
+        self.video_width = video_width
+        self.video_height = video_height
+        self.video_x = (width - video_width) // 2 + 250  # 動画キャンバスのX座標
+        self.video_y = (height - video_height) // 2  # 動画キャンバスのY座標
+        self.video_path = None
+        self.capture = None
+        self.isPlaying = False  # 動画の再生状態を管理するフラグ
+        self.canvas.bind("<Button-1>", lambda event: self.play_pause_video())  # 画面をクリックして再生/停止を切り替える
+        self.canvas.focus_set()
+        self.frame_count = 0
 
+        # キーボードイベントのバインド
+        self.canvas.bind_all("<Left>", lambda event: self.adjust_video_position(-1))
+        self.canvas.bind_all("<Right>", lambda event: self.adjust_video_position(1))
+        self.canvas.bind("<space>", lambda event: self.play_pause_video())
+
+        # アイコンの読み込み
+        self.play_icon = Image.open(r"C:\Users\yuki.shibashi\Desktop\aikon\aikon1.png")
+        self.play_icon2 = Image.open(r"C:\Users\yuki.shibashi\Desktop\aikon\aikon2.png")
+        self.play_icon3 = Image.open(r"C:\Users\yuki.shibashi\Desktop\aikon\aikon3.png")
+        self.play_icon4 = Image.open(r"C:\Users\yuki.shibashi\Desktop\aikon\aikon4.png")
+        self.play_icon = self.play_icon.resize((32, 32))
+        self.play_icon2 = self.play_icon2.resize((32, 32))
+        self.play_icon3 = self.play_icon3.resize((50, 50))
+        self.play_icon4 = self.play_icon4.resize((32, 32))
+
+        self.play_icon = ImageTk.PhotoImage(self.play_icon)
+        self.play_icon2 = ImageTk.PhotoImage(self.play_icon2)
+        self.play_icon3 = ImageTk.PhotoImage(self.play_icon3)
+        self.play_icon4 = ImageTk.PhotoImage(self.play_icon4)
+
+        # ボタンの作成
+        self.load_button = ttk.Button(parent, text="Load Video", command=self.load_video)
+        self.load_button.pack(side=tk.TOP, pady=10)
+
+        self.prev_frame_button = ttk.Button(parent, image=self.play_icon3, command=self.prev_frame)
+        self.prev_frame_button.pack(side=tk.LEFT, padx=10)
+
+        self.next_frame_button = ttk.Button(parent, image=self.play_icon, command=self.next_frame)
+        self.next_frame_button.pack(side=tk.LEFT, padx=10)
+
+        self.save_frame_button = ttk.Button(parent, text="Save Frame as JPEG", command=self.save_current_frame_as_jpeg)
+        self.save_frame_button.pack(side=tk.LEFT, padx=10)
+
+        # フレーム範囲を指定するGUI要素
+        self.frame_range_label = ttk.Label(parent, text="Frame Range:")
+        self.frame_range_label.pack(side=tk.LEFT, padx=10)
+
+        self.start_frame_entry = ttk.Entry(parent)
+        self.start_frame_entry.pack(side=tk.LEFT)
+
+        self.to_label = ttk.Label(parent, text="to")
+        self.to_label.pack(side=tk.LEFT)
+
+        self.end_frame_entry = ttk.Entry(parent)
+        self.end_frame_entry.pack(side=tk.LEFT)
+
+        self.save_frame_range_button = ttk.Button(parent, text="Save Frames in Range", command=self.save_frames_in_range)
+        self.save_frame_range_button.pack(side=tk.LEFT, padx=10)
+
+        # スライダーの追加
+        self.frame_slider = ttk.Scale(parent, from_=0, to=100, orient="horizontal", command=self.on_slider_move)
+        self.frame_slider.pack(side=tk.BOTTOM, fill="x")
+
+        # フレーム数を表示するラベルの追加
+        self.frame_count_label = ttk.Label(parent, text="Total Frames: 0")
+        self.frame_count_label.pack(side=tk.BOTTOM, padx=10)
+        
+           # フレーム数を表示するラベルを作成する
+        self.total_frames_label = tk.Label(parent, text="Total Frames: 0")
+        self.total_frames_label.pack()
+        
+         # ビデオを読み込んでフレーム数を取得し、ラベルに表示する
+    
+    def load_video(self):
+        self.video_path = filedialog.askopenfilename()
+        if self.video_path:
+            self.play_video()
+         
+            capture = cv2.VideoCapture(self.video_path)   
+             # 動画が再生されるときにフレーム数を更新する
+        total_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.total_frames_label.config(text=f"Total Frames: {total_frames}")
+
+
+    
+    def prev_frame(self):
+        self.adjust_video_position(-1)
+    
+    def next_frame(self):
+        self.adjust_video_position(1)
+
+    def save_current_frame_as_jpeg(self):
+        if self.capture:
+            ret, frame = self.capture.read()
+            if ret:
+                filename = filedialog.asksaveasfilename(defaultextension=".jpeg")
+                if filename:
+                    cv2.imwrite(filename, frame)
+
+    def save_frames_in_range(self):
+        if self.capture:
+            selected_directory = filedialog.askdirectory()
+            if not selected_directory:
+                return
+            start_frame = int(self.start_frame_entry.get())
+            end_frame = int(self.end_frame_entry.get())
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+            current_frame = start_frame
+            while current_frame <= end_frame:
+                ret, frame = self.capture.read()
+                if ret:
+                    filename = os.path.join(selected_directory, f"frame_{current_frame}.jpeg")  # Create a unique file name for each frame
+                    cv2.imwrite(filename, frame)  # Save the frame
+                    current_frame += 1
+                else:
+                    break
+
+    def on_slider_move(self, value):
+        if self.capture:
+            frame_pos = float(value)
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, int(frame_pos))
+            ret, frame = self.capture.read()
+            if ret:
+                frame = cv2.resize(frame, (self.video_width, self.video_height))  # Resize video frame
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+                self.canvas.create_image(self.video_x, self.video_y, image=self.photo, anchor=tk.NW)
+
+    def play_video(self):
+        self.capture = cv2.VideoCapture(self.video_path)
+        
+        self.isPlaying = True
+        self.play_frame()
+
+        # 動画が再生されるときにフレーム数を更新する
+        total_frames = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_count_label.config(text=f"Total Frames: {total_frames}")
+
+        # 追加機能：ファイル読み込み後の総フレーム数をラベルに表示する
+        self.total_frames_label.config(text=f"Total Frames: {total_frames}")
+    
+    def play_frame(self):
+        if self.isPlaying:
+            ret, frame = self.capture.read()
+            if ret:
+                frame = cv2.resize(frame, (self.video_width, self.video_height))  # Resize video frame
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+                self.canvas.create_image(self.video_x, self.video_y, image=self.photo, anchor=tk.NW)
+                self.frame_count += 1
+                # フレーム数をラベルに表示
+                self.frame_count_label.config(text=f"Total Frames: {self.frame_count}")
+                self.parent.after(30, self.play_frame)
+
+                # スライダーの最大値を動画のフレーム数に設定
+                total_frames = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+                self.frame_slider.config(to=total_frames)
+
+    def stop_video(self):
+        if self.capture:
+            self.isPlaying = False
+            self.capture.release()
+            self.canvas.delete("all")
+
+    def play_pause_video(self):
+        if self.isPlaying:
+            self.isPlaying = False
+            self.play_frame()
+        else:
+            self.isPlaying = True
+            self.play_frame()
+    
+    def adjust_video_position(self, direction):
+        if self.capture:
+            current_frame_pos = self.capture.get(cv2.CAP_PROP_POS_FRAMES)
+            new_frame_pos = max(0, int(current_frame_pos + direction - 1))
+            print(new_frame_pos)
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, new_frame_pos)
+            ret, frame = self.capture.read()
+            if ret:
+                frame = cv2.resize(frame, (self.video_width, self.video_height))  # Resize video frame
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+                self.canvas.create_image(self.video_x, self.video_y, image=self.photo, anchor=tk.NW)
+
+   
 class CustomThread(threading.Thread):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -50,6 +240,7 @@ class VideoAnalyzer:
         self.row = row
         self.column = column
         self.video_path = ""
+        self.start_frame_number = 0
 
         # キャンバスの作成
         self.canvas = tk.Canvas(parent, bg="black", width=400, height=300)
@@ -134,8 +325,8 @@ class VideoAnalyzer:
                 if  flag == True:
                     break
                 start_frame_number += frame_size
-            start_frame_number = (int)(start_frame_number / frame_size)
-            self.capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
+            self.start_frame_number = (int)(start_frame_number / frame_size)
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, self.start_frame_number)
             while self.stop_thread_flag != "stop":
                 ret, frame = self.capture.read()
                 if not ret:
@@ -154,12 +345,11 @@ class VideoAnalyzer:
                 self.capture.release()
 
 def set_resolution():
+    global resolution
     resolution_str = simpledialog.askstring("解像度を入力", "解像度を幅x高さの形式で入力してください（例：640x480）:")
     if resolution_str:
         width, height = map(int, resolution_str.split("x"))
         resolution = (width, height)
-        # 解像度を設定した後、キャンバスのサイズを更新する
-        video_analyzer.canvas.config(width=width, height=height)
 
 
 duration = 10
@@ -168,9 +358,11 @@ def set_duration():
     duration_str = simpledialog.askstring("動画の長さを入力", "動画の長さ（秒）を入力してください:")
     if duration_str:
         duration = float(duration_str)
-        # 動画の長さを設定
 
+
+second_tab = None
 def handle_plot_and_save():
+    global second_tab
     files = filedialog.askopenfilenames()
     if files:
         for file_path in files:
@@ -230,7 +422,9 @@ def merge_videos(video_analyzers):
         for video_analyzer in video_analyzers:
             if video_analyzer.video_path == "":
                 continue
-            caps.append(cv2.VideoCapture(video_analyzer.video_path))
+            cap = cv2.VideoCapture(video_analyzer.video_path)
+            cap.set(cv2.CAP_PROP_POS_FRAMES, video_analyzer.start_frame_number)  # 120フレーム目から読み込みを開始する
+            caps.append(cap)
             labels.append(video_analyzer.get_input_text())
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         output_video = cv2.VideoWriter('output_video.mp4', fourcc, 30.0, (FRAME_WIDTH * 2, FRAME_HEIGHT * 2))
@@ -282,10 +476,35 @@ def merge_videos(video_analyzers):
                 break
             time.sleep(1 / 33)
 
+def analyze_audio_waveform(video_analyzer):
+    start_frame_number = 0
+    frame_size = (int)(video_analyzer.sample_rate / 30)
+    while True:
+        flag = False
+        for i in range(frame_size):
+            if  video_analyzer.waveforms[start_frame_number + i] > 0.4:
+                flag = True
+                break
+        if  flag == True:
+            break
+        start_frame_number += frame_size
+    start_frame_number = (int)(start_frame_number / frame_size)
+    video_analyzer.capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
+    while video_analyzer.stop_thread_flag != "stop":
+        ret, frame = video_analyzer.capture.read()
+        if not ret:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (400, 300))
+        video_analyzer.canvas.delete("all")
+        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+        video_analyzer.canvas.create_image(0, 0, image=photo, anchor=tk.NW)
+        video_analyzer.parent.update()
+        time.sleep(1 / 30)
 
 def main():
     global duration
-    
+    global second_tab
     window = tk.Tk()
     window.title("Video Helper (Ver0.2)")
 
@@ -324,7 +543,9 @@ def main():
     third_tab = ttk.Frame(tab_control)
     third_tab.pack(expand=True, fill="both")
     tab_control.add(third_tab, text="Video Player")
-
+    
+    video_player = VideoPlayer(third_tab, width=800, height=500, video_width=700, video_height=400)
+    
     fourth_tab = ttk.Frame(tab_control)
     tab_control.add(fourth_tab, text='Merge Videos')
 
@@ -353,4 +574,4 @@ def main():
     window.mainloop()
 
 if __name__ == "__main__":
-    main()
+  main()
